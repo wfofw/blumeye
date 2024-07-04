9d02a0dedbddac23ffa7eb1336d6b2283e5f37b8
wen <uunreall0@gmail.com>
2024-07-05 02:20:03 +0300

    +stable

--- main.py
+++ main.py
@@ -15,37 +15,60 @@ os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
 stop_program = False
 
 # Создание пула потоков для параллельной обработки
-executor = ThreadPoolExecutor(max_workers=38)
+executor = ThreadPoolExecutor(max_workers=15)
+
+class StopProgramException(Exception):
+    pass
 
 # Функция для захвата экрана
-async def screen_capture(monitor):
+def screen_capture(monitor):
+    if stop_program:
+        raise StopProgramException()
     with mss.mss() as sct:
         screenshot = sct.grab(monitor)
         img = np.array(screenshot)
         gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        return gray_image
+        return img, gray_image
 
 # Функция для пороговой обработки
 def apply_threshold(image):
-    _, thresh = cv2.threshold(image, 70, 255, cv2.THRESH_BINARY)
+    if stop_program:
+        raise StopProgramException()
+    _, thresh = cv2.threshold(image, 99, 255, cv2.THRESH_BINARY)
     return thresh
 
 # Функция для нахождения и упрощения контуров
 def find_and_simplify_contours(image):
+    if stop_program:
+        raise StopProgramException()
     contours, _ = cv2.findContours(image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     epsilon = 0.01 * max(cv2.arcLength(cnt, True) for cnt in contours) if contours else 0
     simplified_contours = [cv2.approxPolyDP(cnt, epsilon, True) for cnt in contours]
     return simplified_contours
 
+# Функция для фильтрации прямых контуров
+def filter_straight_contours(contours, min_aspect_ratio=5):
+    filtered_contours = []
+    for contour in contours:
+        x, y, w, h = cv2.boundingRect(contour)
+        if w == 0 or h == 0 or max(w / h, h / w) > min_aspect_ratio:
+            continue
+        filtered_contours.append(contour)
+    return filtered_contours
+
 # Функция для клика по найденной области
 def click_on_contour(contour, monitor):
+    if stop_program:
+        raise StopProgramException()
     x, y, w, h = cv2.boundingRect(contour)
-    center_x = monitor['left'] + x + w // 2
-    center_y = monitor['top'] + y + h // 2
+    center_x = monitor['left'] + x + w / random.uniform(1.210, 1.999)
+    center_y = monitor['top'] + y + h / random.uniform(1.210, 1.999)
     pyautogui.click(center_x, center_y)
 
 # Функция для сохранения скриншотов
 def save_screenshot(image, directory='screenshots', prefix='screenshot'):
+    if stop_program:
+        raise StopProgramException()
     if not os.path.exists(directory):
         os.makedirs(directory)
     timestamp = time.strftime("%Y%m%d-%H%M%S")
@@ -54,6 +77,8 @@ def save_screenshot(image, directory='screenshots', prefix='screenshot'):
 
 # Функция для визуализации контуров
 def visualize_contours(image, contours, matched_contour=None):
+    if stop_program:
+        raise StopProgramException()
     vis_image = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
     cv2.drawContours(vis_image, contours, -1, (0, 255, 0), 2)  # Зеленые контуры
     if matched_contour is not None:
@@ -66,6 +91,26 @@ def on_press(key):
     if hasattr(key, 'char') and key.char == 'q':
         stop_program = True
         return False  # Остановить слушатель
+
+# Функция для обработки скриншота
+def process_screenshot(monitor, random_click):
+    if stop_program:
+        return
+    try:
+        capture_image, gray_image = screen_capture(monitor)
+        capture_thresh = apply_threshold(gray_image)
+        contours = find_and_simplify_contours(capture_thresh)
+        if contours:
+            contours = filter_straight_contours(contours)  # Фильтрация прямых контуров
+            for contour in contours:
+                if 100 < cv2.contourArea(contour) < 10000:
+                    click_on_contour(contour, monitor)
+                    click_fixed_point(*random_click)
+                    vis_image = visualize_contours(capture_thresh, contours, contour)
+                    save_screenshot(vis_image, prefix='matched')
+                    break
+    except StopProgramException:
+        return
 
 # Функция для клика по определенной точке
 def click_fixed_point(x, y):
@@ -87,6 +132,8 @@ async def main():
     listener = keyboard.Listener(on_press=on_press)
     listener.start()
 
+    futures = []
+
     while not stop_program:
         fixed_click_point1 = (random.randint(387, 390) + random.randint(9, 174) // 2, random.randint(1019, 1025) + random.randint(1, 6) // 2)
         fixed_click_point2 = (random.randint(1406, 1446) + random.randint(7, 50) // 2, random.randint(1019, 1025) + random.randint(1, 6) // 2)
@@ -100,22 +147,18 @@ async def main():
             fixed_click_point2,
             random_click_right_side
         ])
-        # Захват экрана и обработка изображения
-        capture_image = await screen_capture(monitor)
-        capture_thresh = apply_threshold(capture_image)
-        contours = find_and_simplify_contours(capture_thresh)
-        if contours is None:
-            continue
 
-        if contours:
-            click_on_contour(contours[0], monitor)
-            # Сохранение скриншота с найденным контуром
-            vis_image = visualize_contours(capture_thresh, contours, contours[0])
-            save_screenshot(vis_image, prefix='matched')
-        else:
-            await asyncio.sleep(random.uniform(0.62, 1.0))
-            click_fixed_point(*random_fixed_click_point)
+        # Запуск задачи в отдельном потоке
+        future = executor.submit(process_screenshot, monitor, random_fixed_click_point)
+        futures.append(future)
 
+        await asyncio.sleep(0.06)
+
+    # Принудительное завершение всех запущенных задач
+    for future in futures:
+        future.cancel()
+
+    executor.shutdown(wait=False)
     cv2.destroyAllWindows()
 
 if __name__ == '__main__':
